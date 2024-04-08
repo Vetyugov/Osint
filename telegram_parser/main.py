e7d45826f4da2d163dc3c4a3cd5469464e551b89
TemaAPI <sludanov.artem@yandex.ru>
2024-04-08 20:35:57 +0300

    Parse Telegram v2.0

--- telegram_parser/main.py
+++ telegram_parser/main.py
@@ -1,18 +1,18 @@
 import time
+import asyncio
+from connect import client
+from search_crypto_in_find_channels import SearchCryptoInFindChannels
 
-start_time = time.time()
+if __name__ == "__main__":
+    start_time = time.time()
+    async def run():
+        await client.start()
+        search_crypto = SearchCryptoInFindChannels()
+        await search_crypto.search()
+        await client.disconnect()
 
-import connect
-import set_parameters
-import search_in_channels
+    asyncio.run(run())
 
-def main():
-    connect
-    set_parameters
-
-if __name__ == '__main__':
-    main()
-
-end_time = time.time()  # Засекаем время окончания выполнения кода
-execution_time = end_time - start_time
-print(f"Время выполнения: {execution_time} секунд(-ы)")
+    end_time = time.time()  # Засекаем время окончания выполнения кода
+    execution_time = end_time - start_time
+    print(f"Время выполнения: {execution_time} секунд(-ы)")
