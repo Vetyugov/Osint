54d8670c746091677b01bda4d0731c07cd294c27
TemaAPI <137876826+TemaAPI@users.noreply.github.com>
2024-05-06 22:07:27 +0300

    Delete telegram_parser/set_parameters.py

--- telegram_parser/set_parameters.py
+++
@@ -1,9 +0,0 @@
-class Parameters:
-    ethereum_pattern = r'\b(0x[a-fA-F0-9]{40})\b'
-    bitcoin_pattern = r'\b(?:bc1|[13])[A-HJ-NP-Z0-9]{25,39}\b'
-    keywords = [
-        'биткоин', 'эфириум', 'криптовалюта', 'блокчейн', 'крипторасследования', 'cryptocurrency investigations', 'bitcoin',
-        'etherium', 'криптобиржа', 'криптогазеты', 'криптоновости', 'криптофорум', 'криптокошельки', 'майнинг',
-        'криптофермы', 'криптомошенничество', 'Криптовалюта', 'Криптоскамеры', 'Криптоскам', 'Cryptoscam',
-        'Crypto fraud', 'Crypto'
-    ]
